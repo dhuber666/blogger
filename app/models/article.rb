5fdd8c0d63847fa259fc0e53b6bec664f9c6fa24
Dominik Huber <dhuber666@gmail.com>
2017-04-20 15:39:36 +0200

    added picture upload - finished

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  has_attached_file :image
+  has_attached_file :image, styles: {medium: "300 x 300>", thumb: "100 x 100>"}
   validates_attachment_content_type :image, :content_type => ["image/jpg",
     "image/jpeg", "image/png"]
 
